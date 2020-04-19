dd2524b3cdeeee728716989a2b727618c271161f
awakone98 <awakone98@users.noreply.github.com>
2020-04-19 10:44:12 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -14,19 +14,19 @@ int main(void)
     int cents = round(dollars * 100);
     // 41 - 25 = 16
     int coins = 0;
-    while (cents > 25 )
+    while (cents >= 25 )
     {
         cents-= 25; 
         coins++;
     }
 
-    while (cents > 10)
+    while (cents >= 10)
     {
         cents-= 10; 
         coins++;
     }
 
-    while (cents > 5)
+    while (cents >= 5)
     {
         cents-= 5; 
         coins++;
