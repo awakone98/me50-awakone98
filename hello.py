b13028d5c867d2f7a20b492afb385e52781e68b7
awakone98 <awakone98@users.noreply.github.com>
2020-04-24 19:56:02 +0000

    automated commit by submit50

---
+++ hello.py
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <get_string.h>
+from cs50 import get_string
+
+#Ask user for name
+name = get_string("What is your name?\n")
+
+# Print hello, users name
+print("hello, \n", name)
+
+   
+
+    
