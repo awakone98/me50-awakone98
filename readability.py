af94d9b9ed7398602d8ad3cb1c79a40100b64eb0
awakone98 <awakone98@users.noreply.github.com>
2020-04-25 18:20:28 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -1,28 +1,35 @@
 from cs50 import get_string
 
-#user input
+# user text input 
 text = get_string("Text: \n")
-    
-def main(void):
-    letterscount += 1;
-    wordcount += 1;
-    sentencecount += 1;
 
-    # letterscount
+letters = 0
+words = 0
+sentences = 0
+counter = 0
+
+for i in text:
+    counter += 1
+
+for i in range(counter):
+    # lettercount
     if (ord(text[i]) >= 65 and ord(text[i]) <= 122):
+        letters += 1
 
-    #  wordcount
+    # wordcount
     if (ord(text[i]) == 32 and (ord(text[i - 1]) != 33 and ord(text[i - 1]) != 46 and ord(text[i - 1]) != 63)):
+        words += 1
 
-    # sentencecount
+    # sentence count
     if (ord(text[i]) == 33 or ord(text[i]) == 46 or ord(text[i]) == 63):
-        
+        sentences += 1
+        words += 1
 
 L = letters * 100 / words
 S = sentences * 100 / words
-i = round(0.0588 * L - 0.296 * S - 15.8)
+index = round(0.0588 * L - 0.296 * S - 15.8)
 
-# output
+# readability output
 if (index < 1):
     print("Before Grade 1")
 
@@ -30,9 +37,7 @@ if (index >= 16):
     print("Grade 16+")
 
 else:
-    print("Grade {index}")
+    print(f"Grade {index}")
     
-if __name__ == "__main__":
-     main(void)
 
 #source https://github.com/me50/awakone98/blob/cs50/problems/2020/x/readability/readability.c
